628e872495f776645e2fa2ec9e076d02eed6dd9b
Hemanth HM <hemanth.hm@gmail.com>
2014-01-05 11:48:55 +0530

    First cut

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+var map = require('map-stream');
+var es = require('event-stream');;
+var gutil = require('gulp-util');
+var htmlClean = require('htmlclean');
+
+module.exports = function() {
+  return es.map(function (file, cb) {
+    file.contents = new Buffer(htmlClean(file.contents.toString()));
+    cb(null,file);
+  });
+};
